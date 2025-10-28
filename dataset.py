fix: CE and EQE calculation errors

dataset.py
@@ -27,7 +27,7 @@ class IITA_Dataset():
     @property
     def eqe(self) -> pd.DataFrame:
         return self._eqe
-    @ce.setter
+    @eqe.setter
     def eqe(self, inp: pd.DataFrame) -> None:
         self._eqe = inp
     equiv_examples = eqe
@@ -43,26 +43,27 @@ class IITA_Dataset():
         self._eqe = None
         
         #counterexamples computation   
-        self.ce = pd.DataFrame(0, index=np.arange(len(self.rp)), columns=np.arange(len(self.rp)))
+        self.ce = pd.DataFrame(0, index=np.arange(self.rp.shape[1]), columns=np.arange(self.rp.shape[1]))
 
         for i in range(len(self.rp)):
-            #for respondent i, find all cases where a=1 and b=0 (counterexamples to a->b) and increment where they intersect
-            a = self.rp.loc[i] == 1
-            not_b = self.rp.loc[i] == 0
-            self.ce.loc[a, not_b] += 1
+            #for respondent i, find all cases where a=0 and b=1 (counterexamples to b->a or a <= b) and increment where they intersect
+            not_a = (self.rp.loc[i] == 1)
+            b = (self.rp.loc[i] == 0)
+            self.ce.loc[not_a, b] += 1
         
         #equivalence examples computation   
+        self.eqe = pd.DataFrame(0, index=np.arange(self.rp.shape[1]), columns=np.arange(self.rp.shape[1]))
         for i in range(len(self.rp)):
             #for respondent i, increment all cases where a=b (examples of equivalence of a and b)
-            row = self.rp.loc[i]
-            self.eqe.loc[np.equal.outer(row, row)] += 1
+            row = self.rp.loc[i].to_numpy()
+            self.eqe += np.equal.outer(row, row).astype(int)
     
     def add(self, dataset_to_add: Self) -> Self:
         """
         Add a second IITA_Dataset: concatenate the response patterns, add counterexamples and equivalence examples\n
         Item amounts must match, else ValueError
         """
-        if (len(self.rp) != len(dataset_to_add.rp)):
+        if (self.rp.shape[1] != dataset_to_add.shape[1]):
             raise ValueError('Item amounts must match')
         
         self.rp = pd.concat(self.rp, dataset_to_add.rp)