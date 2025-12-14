a5c158b2b4569cb9303b37be843f7e41eff784f2
Aliaksei Badnarchuk <alexejbodnarchuk@gmail.com>
2025-12-14 13:37:05 +0100

    fix: only includingpackage folder in build

--- setup.py
+++ setup.py
@@ -1,2 +1,6 @@
-from setuptools import setup
-setup(use_scm_version=True)
+from setuptools import setup, find_packages
+
+setup(
+    use_scm_version=True,
+    packages=find_packages(include=["iita_python*"]),
+)
