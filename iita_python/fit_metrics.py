92fac63d06c3375a329d815231aa915cc32075de
Aliaksei Badnarchuk <alexejbodnarchuk@gmail.com>
2025-12-16 17:51:50 +0100

    feat: minimized iita fit metric

--- iita_python/fit_metrics.py
+++ iita_python/fit_metrics.py
@@ -62,3 +62,42 @@ def corr_iita_fit(data: Dataset, qo: QuasiOrder):
     ce = data.ce.to_numpy().flatten()
     expected_ce = expected_ce.flatten()
     return ((ce - expected_ce) ** 2).sum() / (data.items**2 - data.items)
+
+def mini_iita_fit(data: Dataset, qo: QuasiOrder):
+    """
+    Calculates the minimized IITA fit metric for a given dataset and quasiorder\n
+    """
+    p = data.rp.to_numpy().nansum(axis=0)
+
+    x = [0, 0, 0, 0]
+    for a in range(data.items):
+        for b in range(data.items):
+            if (a == b): continue
+
+            if (qo.full_matrix[a][b]):
+                x[1] += -2 * data.ce.iloc[a, b] * p[b]
+                x[3] += 2 * p[b] ** 2
+            elif (qo.full_matrix[b][a]):
+                x[0] += -2 * data.ce.iloc[a, b] * p[a] + 2 * p[a] * p[b] - 2 * p[a] ** 2
+                x[2] += 2 * p[a] ** 2
+
+    error = - (x[0] + x[1]) / (x[2] + x[3])
+
+    p /= data.subjects
+
+    expected_ce = np.zeros(data.ce.shape)
+
+    for i in range(data.items):
+        for j in range(data.items):
+            if (i == j): continue
+
+            if (qo.full_matrix[i][j]):
+                expected_ce[i][j] = error * p[j] * data.subjects
+            elif (not qo.full_matrix[j][i]):
+                expected_ce[i][j] = (1 - p[i]) * p[j] * data.subjects
+            else:
+                expected_ce[i][j] = (p[j] * data.subjects) - ((p[i] - (p[i] * error)) * data.subjects)
+    
+    ce = data.ce.to_numpy().flatten()
+    expected_ce = expected_ce.flatten()
+    return ((ce - expected_ce) ** 2).sum() / (data.items**2 - data.items)
