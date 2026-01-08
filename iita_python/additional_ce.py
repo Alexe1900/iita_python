3bd69d3f737fba0934e3af1cc9c088f28fb25255
Aliaksei Badnarchuk <alexejbodnarchuk@gmail.com>
2026-01-08 18:03:04 +0100

    feat: added relative CE with free base calculator choice to additional_ce.py

--- iita_python/additional_ce.py
+++ iita_python/additional_ce.py
@@ -1,36 +1,38 @@
 import numpy as np
 import pandas as pd
 
-def general_ce(rp: pd.DataFrame):
+def general_ce(rp: pd.DataFrame) -> pd.DataFrame:
     """
     Computes counterexamples from a response pattern DataFrame\n
     Relies on strict 0/1 values in the response patterns\n
     Supports missing values in the response patterns by ignoring them\n
     """
+
     items = rp.shape[1]
     subjects = rp.shape[0]
 
     ce = pd.DataFrame(0, index=np.arange(items), columns=np.arange(items))
     for i in range(subjects):
-        #for subject i, increment all cases where a=0 and b=1 (counterexamples to b->a or a <= b)
+        #for subject i, increment all cases where items a=0 and b=1 (counterexamples to "b implies a" or a <= b)
         not_a = (rp.loc[i] == 0)
         b = (rp.loc[i] == 1)
         ce.loc[not_a, b] += 1
     
     return ce
 
-def subtraction_ce(rp: pd.DataFrame):
+def pairwise_diff_ce(rp: pd.DataFrame) -> pd.DataFrame:
     """
-    Computes counterexamples from a response pattern DataFrame by using differences of item pairs\n
+    Computes counterexamples from a response pattern DataFrame by using pairwise differences of item correspondences\n
     Can be used for non-binary data because of not relying on strict 0/1 values\n
     Does not support missing values in the response patterns\n
     """
+
     items = rp.shape[1]
     subjects = rp.shape[0]
 
     ce = pd.DataFrame(0, index=np.arange(items), columns=np.arange(items))
     for i in range(subjects):
-        # for subject i, if a < b, add b - a for all pairs (a,b)
+        # for subject i, if a < b, add b - a for all item pairs (a,b)
         # this is equivalent to ce[a][b] += 1 if a=0 and b=1, but works for non-binary data as well
 
         row = rp.loc[i].to_numpy()
@@ -38,12 +40,13 @@ def subtraction_ce(rp: pd.DataFrame):
 
     return ce
 
-def missing_value_substitution_ce(rp: pd.DataFrame):
+def missing_value_substitution_ce(rp: pd.DataFrame) -> pd.DataFrame:
     """
-    Computes counterexamples from a response pattern DataFrame by using differences of item pairs\n
+    Computes counterexamples from a response pattern DataFrame by using pairwise differences of item correspondences\n
     Substitutes missing values in the response patterns with the mean of the item, making some counterexample amounts fractional\n
     Can be used for non-binary data because of not relying on strict 0/1 values\n
     """
+
     items = rp.shape[1]
     subjects = rp.shape[0]
 
@@ -56,4 +59,35 @@ def missing_value_substitution_ce(rp: pd.DataFrame):
         col = pd.Series(col).fillna(mean_val)
         rp1.loc[:, i] = col
 
-    return subtraction_ce(rp1)
+    # then calculate pairwise difference counterexamples
+    return pairwise_diff_ce(rp1)
+
+def relativify(calculator: callable):
+    """
+    Decorator to relativify counterexample calculators\n
+    The counterexample amounts are divided by the number of cases for each item pair where both items are not missing\n
+    """
+
+    def wrapper(rp: pd.DataFrame):
+        f"""
+        Computes counterexamples relative to the amount of valid cases using {calculator.__name__} as base calculator\n
+        The counterexample amounts are divided by the number of cases for each item pair where both items are not missing\n
+        """
+
+        ce = calculator(rp)
+
+        items = rp.shape[1]
+        subjects = rp.shape[0]
+
+        valid_cases = pd.DataFrame(0, index=np.arange(items), columns=np.arange(items))
+        for i in range(subjects):
+            #for subject i, increment all cases where neither a nor b are NaN (valid case for counterexamples)
+            not_nan = np.logical_not(rp.loc[i].isna())
+            valid_cases += np.outer(not_nan, not_nan).astype(int)
+
+        # avoid division by zero
+        valid_cases = valid_cases.replace(0, 1)
+
+        return ce / valid_cases
+
+    return wrapper
